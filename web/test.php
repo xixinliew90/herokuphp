77e97369b9b8d91b86446aaf7a069df78b6d937d
xixinliew90 <20019652@myrp.edu.sg>
2022-07-08 12:33:05 +0800

    Exercise 3 Complete

--- web/test.php
+++ web/test.php
@@ -8,6 +8,11 @@
     <!-- fix : Does not have a <meta name="viewport"> tag with width or initial-scale -->
     <!-- fix : Document doesn't use legible font sizes -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- fix : Document does not have a meta description -->
+    <meta name="description" content="Fix the code to obtain a score of 100 for 
+    Performance, Accessibility, Best Practices and SEO. Comment the fixed code to 
+    reference the issue. Note that you will have to deploy your changes to Heroku 
+    before you do your test.">
     <!-- fix : Document does not have a <title> element -->
     <title>Hello World achieving 100%</title>
 </head>
