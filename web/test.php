b3ab91006a00bfe559934e2a943e08782ffe6855
xixinliew90 <20019652@myrp.edu.sg>
2022-07-08 12:19:52 +0800

    commit message goes here

--- web/test.php
+++ web/test.php
@@ -1,9 +1,14 @@
-<!doctype html>
+<!-- fix : Page lacks the HTML doctype, thus triggering quirks-mode -->
+<!DOCTYPE html>
+<!-- fix : <html> element does not have a [lang] attribute -->
 <html lang="en">
 <html>
 
 <head>
+    <!-- fix : Does not have a <meta name="viewport"> tag with width or initial-scale -->
+    <!-- fix : Document doesn't use legible font sizes -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- fix : Document does not have a <title> element -->
     <title>Hello World achieving 100%</title>
 </head>
 
